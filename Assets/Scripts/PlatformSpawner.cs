df26a25f09a2416b3cb04b34e87d2871a4ab759c
Alina Petrusenko <coolestdoggie1@gmail.com>
2021-06-10 17:25:35 +0200

    objectPooler, Enemy&PlatformSpawner added

---
+++ Assets/Scripts/PlatformSpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformSpawner : MonoBehaviour
+{
+    [SerializeField] private int amountPlatformsOnStart = 6;
+    [SerializeField] private Vector2 offset;
+
+    void Start()
+    {
+        SpawnStartPlatforms();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void SpawnStartPlatforms()
+    {
+        Vector2 pos = new Vector2(0, -3.6f);
+
+        for (int i = 0; i < amountPlatformsOnStart; i++)
+        {
+            GameObject pooledItem = ObjectPooler.SharedInstance.GetPooledObject(Random.Range(0,5));
+            pooledItem.SetActive(true);
+            pooledItem.transform.position = pos;
+
+            pos += offset;
+        }
+    }
+}
---
+++ Assets/Scripts/Platforms.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platforms : MonoBehaviour
+{
+    [SerializeField] GameObject enemy;
+
+
+    private void Start()
+    {
+        enemy.transform.position = new Vector2(Random.Range(-3, 3), enemy.transform.position.y);
+    }
+}
