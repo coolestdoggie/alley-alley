fc9a70a9dedd5a8a0cd18bf5ad2d61968bfa04a0
Alina Petrusenko <coolestdoggie1@gmail.com>
2021-06-11 16:53:55 +0200

    score added

---
+++ Assets/Scripts/GameScreen.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameScreen : MonoBehaviour
+{
+    [SerializeField] TMP_Text scoreText;
+
+    private void Update()
+    {
+        scoreText.text = ScoreCounter.Score.ToString();
+    }
+}
---
+++ Assets/Scripts/ScoreCounter.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class ScoreCounter: MonoBehaviour
+{
+    private static int score;
+    public static int Score { 
+        get => score; 
+        set 
+        {
+            if (value < score)
+            {
+                return;
+            }
+            else
+            {
+                score = value;
+            }
+        } 
+    }
+    private int beginY;
+    
+
+    private void Start()
+    {
+        beginY = (int)transform.position.y;
+    }
+
+    private void Update()
+    {
+        Score = (int)transform.position.y - beginY;
+
+    }
+
+
+}
